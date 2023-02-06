c6878d7d05a00925be43f23d0878e732514b1e88
FQX117 <91921154+FQX117@users.noreply.github.com>
2023-02-06 13:17:13 -0600

    minops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Interview we search the minimal operations
+'''
+
+
+def minOperations(n):
+    '''
+    calculates the fewest number of operations needed to result
+    in exactly n H characters in the file.
+    '''
+    if type(n) is not int or n <= 1:
+        return 0
+
+    H = 2
+    result = 0
+
+    while H <= n:
+        if n % H == 0:
+            '''is the n divisable by H''' 
+            n = n / H
+            result += H
+        else:
+            H += 1
+
+    return result
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+practice form minamil oprations
