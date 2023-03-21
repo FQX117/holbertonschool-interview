68b28d2fe53e9cc11815a7ea67076a0b51e630d2
FQX117 <91921154+FQX117@users.noreply.github.com>
2023-03-21 11:55:05 -0500

    fix

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -9,51 +9,43 @@ struct ListNode {
 
 typedef struct ListNode listint_t;
 
-/* Function to create a new node with given data */
-listint_t* new_node(int data)
-{
-    listint_t* node = (listint_t*) malloc(sizeof(listint_t));
-    node->val = data;
-    node->next = NULL;
-    return node;
-}
-
-/* Function to reverse a linked list */
-listint_t* reverse_list(listint_t* head)
-{
-    listint_t *prev = NULL, *curr = head, *next = NULL;
-    while (curr != NULL) {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return prev;
-}
-
 /* Function to check if a linked list is a palindrome */
 int is_palindrome(listint_t** head)
 {
-    listint_t *slow = *head, *fast = *head;
+    if (*head == NULL) {
+        /* An empty list is considered a palindrome */
+        return 1;
+    }
+
+    /* Find the middle node of the linked list */
+    listint_t* slow = *head;
+    listint_t* fast = *head;
     while (fast != NULL && fast->next != NULL) {
         slow = slow->next;
         fast = fast->next->next;
     }
 
-    if (fast != NULL) {
-        slow = slow->next;
+    /* Reverse the second half of the linked list */
+    listint_t* prev = NULL;
+    listint_t* curr = slow;
+    while (curr != NULL) {
+        listint_t* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
 
-    slow = reverse_list(slow);
-    fast = *head;
-
-    while (slow != NULL) {
-        if (fast->val != slow->val) {
+    /* Compare the first half of the linked list with the reversed second half */
+    listint_t* p1 = *head;
+    listint_t* p2 = prev;
+    while (p2 != NULL) {
+        if (p1->val != p2->val) {
             return 0;
         }
-        fast = fast->next;
-        slow = slow->next;
+        p1 = p1->next;
+        p2 = p2->next;
     }
 
+    /* If we reach this point, the linked list is a palindrome */
     return 1;
 }
